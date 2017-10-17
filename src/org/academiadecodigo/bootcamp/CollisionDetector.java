027571a7e638b7aaa7a18414fd02e630daf5bbde
Milena Balerine <mibalerine@gmail.com>
2017-10-17 18:52:57 +0100

    changed batata to bullet

--- src/org/academiadecodigo/bootcamp/CollisionDetector.java
+++ src/org/academiadecodigo/bootcamp/CollisionDetector.java
@@ -8,18 +8,18 @@ import org.academiadecodigo.bootcamp.entity.GameEntity;
 public class CollisionDetector {
 
     private GameEntity[] gameEntities;
-    private Batata batata;
+    private Bullet bullet;
 
-    public CollisionDetector(GameEntity[] gameEntities, Batata batata) {
+    public CollisionDetector(GameEntity[] gameEntities, Bullet bullet) {
         this.gameEntities = gameEntities;
-        this.batata = batata;
+        this.bullet = bullet;
     }
 
     public void check() {
 
         for (GameEntity entity : gameEntities) {
 
-            if(entity.getGridPosition().equals(batata.getGridPosition())) {
+            if(entity.getGridPosition().equals(bullet.getGridPosition())) {
                 entity.getShot();
             }
 
