2806e1e0a29042553860ed85d9eb199e7656f793
Guilherme Barros <guilhermeandre104@gmail.com>
2017-10-20 16:09:41 +0100

    Level screen added, not implemented

---
+++ src/org/academiadecodigo/bootcamp/LevelScreen.java
@@ -0,0 +1,64 @@
+package org.academiadecodigo.bootcamp;
+
+import org.academiadecodigo.bootcamp.grid.Grid;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+/**
+ * Created by codecadet on 20/10/2017.
+ */
+public class LevelScreen {
+
+    //Properties
+    private Picture fries;
+    private Picture fries2;
+    private Picture fries3;
+    private Picture text;
+    private Picture text2;
+    private Picture potatoes;
+    private Picture potatoes2;
+    private Picture potatoes3;
+
+
+    //Constructor
+    public LevelScreen(Grid grid) {
+        this.fries = new Picture(Grid.PADDING - 33, grid.getHeight() - 157 + Grid.PADDING, "images/french-fries.png");
+        this.fries.grow(-33, 0);
+        this.fries2 = new Picture(500 - 33 - 23, grid.getHeight() - 157 + Grid.PADDING, "images/french-fries.png");
+        this.fries3 = new Picture(1000 - 33 - 40, grid.getHeight() - 157 + Grid.PADDING, "images/french-fries.png");
+        this.fries3.grow(-17, 0);
+        this.text = new Picture(130, 100, "images/Collecting-potatoes.png");
+        this.text2 = new Picture(130, 220, "images/for-next-level.png");
+        this.potatoes = new Picture(150 + Grid.PADDING, 400, "images/potatoes.png");
+        this.potatoes2 = new Picture(grid.getWidth() / 2 - 150 + Grid.PADDING, 400, "images/potatoes.png");
+        this.potatoes3 = new Picture(grid.getWidth() - 450 + Grid.PADDING, 400, "images/potatoes.png");
+        this.fries.draw();
+        this.fries2.draw();
+        this.fries3.draw();
+        this.text.draw();
+        this.text2.draw();
+        potatoLoader();
+    }
+
+    //Methods
+    public void potatoLoader() {
+        Picture[] potatoArray = new Picture[]{potatoes, potatoes2, potatoes3};
+
+        try {
+            for (Picture p : potatoArray) {
+                Thread.sleep(1500);
+                p.draw();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
--- src/org/academiadecodigo/bootcamp/Main.java
+++ src/org/academiadecodigo/bootcamp/Main.java
@@ -1,5 +1,7 @@
 package org.academiadecodigo.bootcamp;
 
+import org.academiadecodigo.bootcamp.grid.Grid;
+
 /**
  * Created by codecadet on 15/10/2017.
  */
@@ -7,9 +9,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Game game = new Game();
+        //Game game = new Game();
 
+        Grid grid = new Grid(1400,900);
 
+        LevelScreen levelScreen = new LevelScreen(grid);
 
         //TODO: MISSILE
     }
