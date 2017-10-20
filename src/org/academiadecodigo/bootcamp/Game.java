119442fe3dec761642050a14748917cec286ce8d
Guilherme Brehm <guilherme.brehm@protonmail.com>
2017-10-20 14:40:52 +0100

    removed some properties from Game that didn't need to  be properties

--- src/org/academiadecodigo/bootcamp/Game.java
+++ src/org/academiadecodigo/bootcamp/Game.java
@@ -14,13 +14,10 @@ public class Game {
 
     private Grid grid;
     private ArrayList<Movable> movables;
-    private ArrayList<Shootable> shootables;
-    private GameEntityFactory gameEntityFactory;
     public static CollisionDetector collisionDetector;
     private InitialScreen initialScreen;
     private KeyboardHandler kBH;
     public static int NUM_LEVELS = 1;
-    private FinalScreen finalScreen;
 
     //Constructor
     public Game() {
@@ -60,20 +57,19 @@ public class Game {
 
         }
 
-        //TODO:insert final screen
-        finalScreen = new FinalScreen((this.grid.getWidth()/2) - 457,10);
+        FinalScreen finalScreen = new FinalScreen((this.grid.getWidth() / 2) - 457, 10);
     }
 
     public void init() {
 
         this.grid = new Grid(1400, 900);
-        this.gameEntityFactory = new GameEntityFactory();
+        GameEntityFactory gameEntityFactory = new GameEntityFactory();
 
         GameLevel gameLevel = new GameLevel("levels/0.lvl");
 
         ArrayList<CodeCadet> codeCadets = gameEntityFactory.getCodeCadets(gameLevel.getCadetArray(), grid);
         movables = new ArrayList<>(codeCadets);
-        shootables = new ArrayList<>(codeCadets);
+        ArrayList<Shootable> shootables = new ArrayList<>(codeCadets);
 
         MC mc = new MC(grid);
         movables.add(mc);
