ab36c9614fdf2d39f723b4a6af506ffa1dd56cbe
Guilherme Brehm <guilherme.brehm@protonmail.com>
2017-10-24 12:20:07 +0100

    started working on a screenmanager

---
+++ src/org/academiadecodigo/bootcamp/screens/ScreenManager.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.bootcamp.screens;
+
+/**
+ * Created by codecadet on 24/10/2017.
+ */
+public class ScreenManager {
+
+}
---
+++ src/org/academiadecodigo/bootcamp/screens/ScreenType.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.bootcamp.screens;
+
+/**
+ * Created by codecadet on 24/10/2017.
+ */
+public enum  ScreenType {
+
+
+}
